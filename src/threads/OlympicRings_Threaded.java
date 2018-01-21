8958baaa0bdba1a49802700cc788cbe49333f15e
GamingAuthor <league@WTS19.attlocal.net>
2018-01-21 11:27:38 -0800

    245789fghjk

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,7 +1,18 @@
 package threads;
 
+import java.awt.AWTException;
+import org.jointheleague.graphical.robot.Robot;
+
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+public static void main(String[] args) {
 
+		Robot blue = new Robot(400,150);
+		Robot black = new Robot(700,150);
+		Robot red = new Robot(1000, 150);
+		Robot yellow = new Robot(550,300);
+		Robot green = new Robot(850,300);
+
+}
 }
 
--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -1,7 +1,31 @@
+package threads;
 
-//public class ThreadedReverseGreeting {
-  //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
-  //Thread 2 creates Thread 3; and so on, up to Thread 50. 
-  //Each thread should print "Hello from Thread <num>!", 
-  //but you should structure your program such that the threads print their greetings in reverse order.
-//}
+
+public class ThreadedReverseGreeting {
+	public static void main(String[] args) {
+		 Thread thread1 = new Thread(()-> doStuff(0));
+thread1.start();
+
+		
+	}
+ 
+  public static void doStuff(int counter) {
+	  if(counter != 51) {
+	 Thread nextThread = new Thread(()-> doStuff(counter+1));
+	 nextThread.start();
+	 try {
+		nextThread.join();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	 System.out.println("Hello from Thread " + counter);
+  }
+	  else {
+		  
+	  }
+}}
+//Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
+//Thread 2 creates Thread 3; and so on, up to Thread 50. 
+//Each thread should print "Hello from Thread <num>!", 
+//but you should structure your program such that the threads print their greetings in reverse order.
