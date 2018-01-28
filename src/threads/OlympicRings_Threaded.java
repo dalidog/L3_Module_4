e5efdecf58c98fcb87f46b20a6a8ed4117cb072a
Aaron Glick <league@WTS19.attlocal.net>
2018-01-28 11:27:07 -0800

    fdasfdsa

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,6 +1,8 @@
 package threads;
 
 import java.awt.AWTException;
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
@@ -8,11 +10,66 @@ public class OlympicRings_Threaded {
 public static void main(String[] args) {
 
 		Robot blue = new Robot(400,150);
+		blue.setSpeed(10);
+		blue.setPenColor(Color.blue);
+		blue.penDown();
+		
 		Robot black = new Robot(700,150);
+		black.setSpeed(10);
+		black.setPenColor(Color.black);
+		black.penDown();
+		
 		Robot red = new Robot(1000, 150);
+		red.setSpeed(10);
+		red.setPenColor(Color.red);
+		red.penDown();
+		
 		Robot yellow = new Robot(550,300);
+		yellow.setSpeed(10);
+		yellow.setPenColor(Color.yellow);
+		yellow.penDown();
+		
 		Robot green = new Robot(850,300);
+		green.setSpeed(10);
+		green.setPenColor(Color.green);
+		green.penDown();
 
-}
-}
+	
+
+		new Thread(()->{
+			for (int i = 0; i < 360; i++) {
+			blue.turn(1);
+			blue.move(2);
+			}
+		}).start();
+		
+		new Thread(()->{
+			for (int i = 0; i < 360; i++) {
+			black.turn(1);
+			black.move(2);
+			}
+		}).start();
+
+		new Thread(()->{
+			for (int i = 0; i < 360; i++) {
+			red.turn(1);
+			red.move(2);
+			}
+		}).start();
+
+		new Thread(()->{
+			for (int i = 0; i < 360; i++) {
+			yellow.turn(1);
+			yellow.move(2);
+			}
+		}).start();
+		
+		new Thread(()->{
+			for (int i = 0; i < 360; i++) {
+			green.turn(1);
+			green.move(2);
+			}
+		}).start();
+}}
+
 
